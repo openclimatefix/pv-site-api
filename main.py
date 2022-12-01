60da53f50bde30f993cbc11eeb83c3be946381ae
rachel-labri-tipton <rachemtipton@gmail.com>
2022-12-01 16:27:02 +0100

    feat: added models to the routes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import logging  
+from pydantic import Field, BaseModel
+from pydantic_models import Forecast, Forecast_Metadata, PV_Site_Metadata, One_PV_Actual, Multiple_PV_Actual, PV_Sites, PVSiteAPIStatus
 
 app = FastAPI()
 
@@ -33,38 +35,38 @@ async def root():    return {"message": "This is the site_specific_PV_API under 
 
 # get_sites: Clients get the site id that are available to them
 @app.get("/sites/site_list")
-async def get_sites(site_id):
-    return {"message": "client gets site list"}
+async def get_sites(pv_sites: PV_Sites):
+    return {"pv_sites": PV_Sites}
 
-# post_pv_actual: sends data to us, and we save to database
+# # post_pv_actual: sends data to us, and we save to database
 @app.post("/sites/pv_actual/{site_id}")
-async def post_pv_actual(site_id):
-    return {"message": "post pv actual"}
+async def post_pv_actual(one_pv_actual: One_PV_Actual):
+    return {"one_pv_actual": One_PV_Actual}
 
 # put_site_info: client can update a site
 @app.put("/sites/pv_actual/{site_id}/info")
-async def put_site_info(site_id):
-    return {"message": "post pv actual"}
+async def put_site_info(site_metadata: PV_Site_Metadata):
+    return {"site_metadata": PV_Site_Metadata}
 
 # get_pv_actual: the client can read pv data from the past
 @app.get("/sites/pv_actual/{site_id}")
-async def get_PV_actual (site_id):
-    return {"message": "read pv data"}
+async def get_PV_actual (one_pv_actual: One_PV_Actual):
+    return {"one_pv_actual": One_PV_Actual}
 
 # get_forecast: Client gets the forecast for their site
 @app.get("/sites/pv_forecast/{site_id}}")
-async def get_PV_forecast(site_id):
-    return {"message": "client gets forecast for their site"}
+async def get_PV_forecast(forecast: Forecast, forecast_metadata: Forecast_Metadata):
+    return {"forecast": Forecast, "forecast_metadata": Forecast_Metadata}
 
 # get_forecast_metadata: Get when the forecast is made, what site is, forecast version
 @app.get("/sites/pv_forecast/metadata/{site_id}}")
-async def get_forecast_metadata(site_id):
-    return {"message": "client gets forecast metadata"}
+async def get_forecast_metadata(forecast_metadata: Forecast_Metadata):
+    return {"forecast_metadata": Forecast_Metadata}
 
 # get_status: get the status of the system
 @app.get("/api_status")
-async def get_status(): 
-    return {"message": "api is running"}
+async def get_status(api_status: PVSiteAPIStatus): 
+    return {"api_status": PVSiteAPIStatus}
 
 
 
--- pydantic_models.py
+++ pydantic_models.py
@@ -1,7 +1,6 @@
 # import packages
 from pydantic import Field, BaseModel
 
-
 # initiate the classes
 
 # get_status
@@ -32,12 +31,11 @@ class One_PV_Actual(BaseModel):
     datetime_utc: str = Field(..., description="Date of data input")
     actual_generation_kw: float = Field(..., description="The site's capacity in kw", ge=0)
 
-
 # get client pv data history for multiple dates
 class Multiple_PV_Actual(BaseModel):
     """Actual PV history for one site (multiple inputs)"""
     site_uuid: str = Field(..., description="The uuid for the sites") 
-    actual_values: list ([One_PV_Actual]) =  Field(..., description="Actual values for multiple sites") 
+    actual_values: str =  Field(..., description="Actual values for multiple sites") 
 
 
 # this wasn't in the write-up, but I think there is a ForecastValues class in the other API and thought this would work
@@ -45,17 +43,17 @@ class Site_Forecast_Values(BaseModel):
   """Forecast value list"""
   forecast_uuid: str = Field(..., description="The forecast id")
   target_datetime_utc: str = Field(..., description="Target time for forecast")
-  expected_generation_kw: str = Field(..., description="Expected generation in kw", ge=0)
+  expected_generation_kw: float = Field(..., description="Expected generation in kw", ge=0)
 
 # get_forecast
 #client gets a forecast for their site
 class Forecast(BaseModel):
   forecast_uuid: str = Field(..., description="The forecast id")
-  forecast_values: list(Site_Forecast_Values) = Field(..., description="List of target times and generation")
+  forecast_values: str = Field(..., description="List of target times and generation")
 
 # client gets information about the forecast: which site, which forecast version, when it was made
 # parameters: site_uuid
-class Forecast_Metadata(EnhancedBaseModel):
+class Forecast_Metadata(BaseModel):
   """Forecast with site information"""
   forecast_metadata_uuid: str = Field(..., description="The forecast id")
   site_uuid: str = Field(..., description="The site id")
@@ -65,5 +63,5 @@ class Forecast_Metadata(EnhancedBaseModel):
 # get_sites
 # this gives the sites available to the client and uses the PV_Site_Metadata from above
 class PV_Sites(BaseModel):
-  site_list: list([PV_Site_Metadata]) = Field(..., description="List of all sites with their metadata")
+  site_list: str = Field(..., description="List of all sites with their metadata")
 
