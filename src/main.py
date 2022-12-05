7fa27a0d398e46ea0d341488925e38e2c275775b
peterdudfield <peter.dudfield@hotmail.com>
2022-12-05 18:32:01 +0000

    run blacks

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
-import logging  
+import logging
 from pydantic import Field, BaseModel
-from pydantic_models import Forecast, Forecast_Metadata, PV_Site_Metadata, One_PV_Actual, Multiple_PV_Actual, PV_Sites, PVSiteAPIStatus
+from pydantic_models import (
+    Forecast,
+    Forecast_Metadata,
+    PV_Site_Metadata,
+    One_PV_Actual,
+    Multiple_PV_Actual,
+    PV_Sites,
+    PVSiteAPIStatus,
+)
 
 app = FastAPI()
 
-version = '0.0.1'
+version = "0.0.1"
+
 
 @app.get("/")
 async def get_api_information():
@@ -23,11 +32,14 @@ async def get_api_information():
         "documentation": "",
     }
 
-@app.get("/")
-async def root():    return {"message": "This is the site_specific_PV_API under construction"}
 
-# name the api 
-# test that the routes are there on swagger 
+@app.get("/")
+async def root():
+    return {"message": "This is the site_specific_PV_API under construction"}
+
+
+# name the api
+# test that the routes are there on swagger
 # Following on from #1 now will be good to set out models
 
 # User story
@@ -40,36 +52,38 @@ async def root():    return {"message": "This is the site_specific_PV_API under 
 async def get_sites(pv_sites: PV_Sites):
     return {"pv_sites": PV_Sites}
 
+
 # # post_pv_actual: sends data to us, and we save to database
 @app.post("/sites/pv_actual/{site_id}")
 async def post_pv_actual(one_pv_actual: One_PV_Actual):
     return {"one_pv_actual": One_PV_Actual}
+
 
 # put_site_info: client can update a site
 @app.put("/sites/pv_actual/{site_id}/info")
 async def put_site_info(site_metadata: PV_Site_Metadata):
     return {"site_metadata": PV_Site_Metadata}
 
+
 # get_pv_actual: the client can read pv data from the past
 @app.get("/sites/pv_actual/{site_id}")
-async def get_PV_actual (one_pv_actual: One_PV_Actual):
+async def get_PV_actual(one_pv_actual: One_PV_Actual):
     return {"one_pv_actual": One_PV_Actual}
+
 
 # get_forecast: Client gets the forecast for their site
 @app.get("/sites/pv_forecast/{site_id}}")
 async def get_PV_forecast(forecast: Forecast, forecast_metadata: Forecast_Metadata):
     return {"forecast": Forecast, "forecast_metadata": Forecast_Metadata}
 
+
 # get_forecast_metadata: Get when the forecast is made, what site is, forecast version
 @app.get("/sites/pv_forecast/metadata/{site_id}}")
 async def get_forecast_metadata(forecast_metadata: Forecast_Metadata):
     return {"forecast_metadata": Forecast_Metadata}
 
+
 # get_status: get the status of the system
 @app.get("/api_status")
-async def get_status(api_status: PVSiteAPIStatus): 
+async def get_status(api_status: PVSiteAPIStatus):
     return {"api_status": PVSiteAPIStatus}
-
-
-
-
