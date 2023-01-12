1db43d6321ec1bc791c7af1cebf6b68f9fa6e564
peterdudfield <peter.dudfield@hotmail.com>
2023-01-12 15:03:01 +0000

    isort . and blacks

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 """Main API Routes"""
-from fastapi import FastAPI
 import logging
 from datetime import datetime, timezone
 from uuid import uuid4
-from pydantic import Field, BaseModel
+
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
 from pydantic_models import (
     Forecast,
-    PVSiteMetadata,
     MultiplePVActual,
-    PVSites,
+    PVActualValue,
     PVSiteAPIStatus,
+    PVSiteMetadata,
+    PVSites,
     SiteForecastValues,
-    PVActualValue
 )
 from utils import make_fake_intensity
-import pandas as pd
 
 app = FastAPI()
 
@@ -27,7 +29,7 @@ fake_site_uuid = "b97f68cd-50e0-49bb-a850-108d4a9f7b7e"
 @app.get("/")
 async def get_api_information():
     """
-    ###  This route returns basic information about the Nowcasting PV Site API. 
+    ###  This route returns basic information about the Nowcasting PV Site API.
 
     """
 
@@ -49,24 +51,25 @@ async def get_api_information():
 # Could look at 'get_forecast_metadata' to see when this forecast is made
 # get_sites: Clients get the site id that are available to them
 
+
 @app.get("/sites/site_list", response_model=PVSites)
 async def get_sites(client_uuid: str):
-    """ 
-    ### This route returns a list of the user's PV Sites with metadata for each site. 
+    """
+    ### This route returns a list of the user's PV Sites with metadata for each site.
     """
     pv_site = PVSiteMetadata(
-        site_uuid=fake_site_uuid, 
-        client_uuid = client_uuid,
-        client_site_id = "the site id used by the user",
-        client_site_name="the site name", 
-        region="the site's region", 
-        dno="the site's dno", 
-        gsp="the site's gsp", 
-        latitude=50, 
-        longitude=0, 
+        site_uuid=fake_site_uuid,
+        client_uuid=client_uuid,
+        client_site_id="the site id used by the user",
+        client_site_name="the site name",
+        region="the site's region",
+        dno="the site's dno",
+        gsp="the site's gsp",
+        latitude=50,
+        longitude=0,
         installed_capacity_kw=1,
         created_utc="datetime.now(timezone.utc)",
-        updated_utc="datetime.now(timezone.utc)"
+        updated_utc="datetime.now(timezone.utc)",
     )
     pv_site_list = PVSites(
         site_list=[pv_site],
@@ -77,11 +80,14 @@ async def get_sites(client_uuid: str):
 
 # post_pv_actual: sends data to us, and we save to database
 @app.post("/sites/pv_actual/{site_uuid}")
-async def post_pv_actual(site_uuid: str,pv_actual: MultiplePVActual,):
-    """ ### This route is used to input the actual PV generation for one user PV site.
-     
-    Users will upload actual PV generation readings at regular intervals throughout a given day.  
-    Currently this route does not return anything. 
+async def post_pv_actual(
+    site_uuid: str,
+    pv_actual: MultiplePVActual,
+):
+    """### This route is used to input the actual PV generation for one user PV site.
+
+    Users will upload actual PV generation readings at regular intervals throughout a given day.
+    Currently this route does not return anything.
     """
     print(f"Got {pv_actual.dict()} for site {site_uuid}")
     print("Not doing anything with it (yet!)")
@@ -90,11 +96,11 @@ async def post_pv_actual(site_uuid: str,pv_actual: MultiplePVActual,):
 # put_site_info: client can update a site
 @app.put("/sites/pv_actual/{site_uuid}/info")
 async def put_site_info(site_info: PVSiteMetadata):
-    """ 
-    ### This route allows a user to update site information for a single site. 
+    """
+    ### This route allows a user to update site information for a single site.
 
     """
-  
+
     print(f"Successfully updated {site_info.dict()} for site {site_info.site_name}")
     print("Not doing anything with it (yet!)")
 
@@ -102,27 +108,25 @@ async def put_site_info(site_info: PVSiteMetadata):
 # get_pv_actual: the client can read pv data from the past
 @app.get("/sites/pv_actual/{site_uuid}", response_model=MultiplePVActual)
 async def get_pv_actual(site_uuid: str):
-    """ ### This route returns PV readings from a single PV site. 
+    """### This route returns PV readings from a single PV site.
 
-    Currently the route is set to provide a reading every hour for the previous 24-hour period. 
+    Currently the route is set to provide a reading every hour for the previous 24-hour period.
     To test the route, you can input any number for the site_uuid (ex. 567)
     to generate a list of datetimes and actual kw generation for that site.
     """
     previous_day = pd.Timestamp((datetime.now(timezone.utc)) - (pd.Timedelta(hours=24))).ceil("5T")
-    datetimes = [previous_day + pd.Timedelta(hours=(i*1)) for i in range(0, 24)]
-    
+    datetimes = [previous_day + pd.Timedelta(hours=(i * 1)) for i in range(0, 24)]
+
     pv_actual_values = []
     for d in datetimes:
         pv_actual_value = PVActualValue(
-            datetime_utc=d, 
-            actual_generation_kw=make_fake_intensity(datetime_utc=d)
+            datetime_utc=d, actual_generation_kw=make_fake_intensity(datetime_utc=d)
         )
         pv_actual_values.append(pv_actual_value)
 
     # make fake iteration of pv values for one day at a specific site
     fake_pv_actual_iteration = MultiplePVActual(
-        site_uuid=site_uuid,
-        pv_actual_values = pv_actual_values
+        site_uuid=site_uuid, pv_actual_values=pv_actual_values
     )
 
     return fake_pv_actual_iteration
@@ -131,15 +135,15 @@ async def get_pv_actual(site_uuid: str):
 # get_forecast: Client gets the forecast for their site
 @app.get("/sites/pv_forecast/{site_uuid}", response_model=Forecast)
 async def get_pv_forecast(site_uuid: str):
-    """ 
+    """
     ### This route is where you might say the magic happens; it returns the PV forecast for a user's PV Site.
-    
-    The forecast is attached to the **site_uuid** and provides a list of forecast values with a 
-    **target_date_time_utc** and **expected_generation_kw** reading every half-hour 8-hours into the future. 
 
-    You can currently input any number for **site_uuid** (ex. 567), 
-    and the route returns a sample forecast. 
-    
+    The forecast is attached to the **site_uuid** and provides a list of forecast values with a
+    **target_date_time_utc** and **expected_generation_kw** reading every half-hour 8-hours into the future.
+
+    You can currently input any number for **site_uuid** (ex. 567),
+    and the route returns a sample forecast.
+
     """
     # timestamps
     now = pd.Timestamp(datetime.now(timezone.utc)).ceil("5T")
@@ -149,8 +153,7 @@ async def get_pv_forecast(site_uuid: str):
     forecast_values = []
     for d in datetimes:
         forecast_value = SiteForecastValues(
-            target_datetime_utc=d, 
-            expected_generation_kw=make_fake_intensity(datetime_utc=d)
+            target_datetime_utc=d, expected_generation_kw=make_fake_intensity(datetime_utc=d)
         )
         forecast_values.append(forecast_value)
 
@@ -165,17 +168,18 @@ async def get_pv_forecast(site_uuid: str):
 
     return fake_forecast
 
+
 # get_status: get the status of the system
 @app.get("/api_status", response_model=PVSiteAPIStatus)
 async def get_status():
-    """ This route gets the status of the system. 
-    
-    It's mostly used by OCF to 
+    """This route gets the status of the system.
+
+    It's mostly used by OCF to
     make sure things are running smoothly.
     """
     pv_api_status = PVSiteAPIStatus(
-        status="ok", 
-        message="The API is up and running", 
-        )
+        status="ok",
+        message="The API is up and running",
+    )
 
     return pv_api_status
--- src/pydantic_models.py
+++ src/pydantic_models.py
@@ -1,14 +1,16 @@
 """Pydantic models for PV Site API"""
 # import packages
-from pydantic import Field, BaseModel
 from datetime import datetime
 from typing import List, Optional
+
+from pydantic import BaseModel, Field
 
 # initiate the classes
 
 # get_status
 class PVSiteAPIStatus(BaseModel):
     """PVSiteAPI Status"""
+
     status: str = Field(..., description="Status description")
     message: str = Field(..., description="Status Message")
 
@@ -20,14 +22,22 @@ class PVSiteMetadata(BaseModel):
     """Site metadata"""
 
     site_uuid: str = Field(..., description="The unique internal ID designated to the site.")
-    client_uuid: str = Field(..., description="The unique internal ID of the user providing the site data.")
+    client_uuid: str = Field(
+        ..., description="The unique internal ID of the user providing the site data."
+    )
     client_site_id: str = Field(..., description="The site ID as given by the providing user.")
-    client_site_name: str = Field(..., decription="The name of the site as given by the providing uuser.")
+    client_site_name: str = Field(
+        ..., decription="The name of the site as given by the providing uuser."
+    )
     region: Optional[str] = Field(None, decription="The region of the PV site")
-    dno: Optional[str]= Field(None, decription="The distribution network operator of the PV site.")
-    gsp: Optional[str]= Field(None, decription="The grid supply point of the PV site.")
-    orientation: Optional[float] = Field(None, description="The rotation of the panel in degrees. 180° points south")
-    tilt:  Optional[float] = Field(None, description="The tile of the panel in degrees. 90° indicates the panel is vertical.")
+    dno: Optional[str] = Field(None, decription="The distribution network operator of the PV site.")
+    gsp: Optional[str] = Field(None, decription="The grid supply point of the PV site.")
+    orientation: Optional[float] = Field(
+        None, description="The rotation of the panel in degrees. 180° points south"
+    )
+    tilt: Optional[float] = Field(
+        None, description="The tile of the panel in degrees. 90° indicates the panel is vertical."
+    )
     latitude: float = Field(..., description="The site's latitude", ge=-90, le=90)
     longitude: float = Field(..., description="The site's longitude", ge=-180, le=180)
     installed_capacity_kw: float = Field(..., description="The site's capacity in kw", ge=0)
@@ -52,8 +62,8 @@ class MultiplePVActual(BaseModel):
 
     site_uuid: str = Field(..., description="The site id")
     pv_actual_values: List[PVActualValue] = Field(
-         ..., description="List of  datetimes and generation"
-     )
+        ..., description="List of  datetimes and generation"
+    )
 
 
 # this wasn't in the write-up, but I think there is a ForecastValues class in the other API and thought this would work
@@ -69,17 +79,23 @@ class SiteForecastValues(BaseModel):
 # client gets a forecast for their site
 class Forecast(BaseModel):
     """PV Forecast"""
+
     forecast_uuid: str = Field(..., description="The forecast id")
     site_uuid: str = Field(..., description="The site id")
-    forecast_creation_datetime: datetime = Field(..., description="The time that the forecast was created.")
+    forecast_creation_datetime: datetime = Field(
+        ..., description="The time that the forecast was created."
+    )
     forecast_version: str = Field(..., description="Forecast version")
     forecast_values: List[SiteForecastValues] = Field(
-         ..., description="List of target times and generation"
-     )
+        ..., description="List of target times and generation"
+    )
 
 
 # get_sites
 # this gives the sites available to the client and uses the PV_Site_Metadata from above
 class PVSites(BaseModel):
     """PV Sites"""
-    site_list: List[PVSiteMetadata] = Field(..., description="List of all sites with their metadata")
+
+    site_list: List[PVSiteMetadata] = Field(
+        ..., description="List of all sites with their metadata"
+    )
