ae1af6652d376aca47b19e12297261bfb54988dc
braddf <bradbdf@gmail.com>
2023-03-03 11:44:41 +0000

    install & add sentry init config

--- pv_site_api/main.py
+++ pv_site_api/main.py
@@ -4,6 +4,7 @@ import os
 import uuid
 
 import pandas as pd
+import sentry_sdk
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -40,6 +41,31 @@ logging.basicConfig(
     format="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
+
+
+def traces_sampler(sampling_context):
+    # Examine provided context data (including parent decision, if any)
+    # along with anything in the global namespace to compute the sample rate
+    # or sampling decision for this transaction
+
+    if os.getenv("ENVIRONMENT") == "local":
+        return 0.0
+    elif "error" in sampling_context["transaction_context"]["name"]:
+        # These are important - take a big sample
+        return 1.0
+    elif sampling_context["parent_sampled"] is True:
+        # These aren't something worth tracking - drop all transactions like this
+        return 0.0
+    else:
+        # Default sample rate
+        return 0.05
+
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    environment=os.getenv("ENVIRONMENT", "local"),
+    traces_sampler=traces_sampler,
+)
 
 app = FastAPI(docs_url="/swagger", redoc_url=None)
 
@@ -342,6 +368,10 @@ async def redoc_html():
     return get_redoc_html_with_theme(
         title=title,
     )
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
 
 
 # OpenAPI (ReDoc) custom theme
