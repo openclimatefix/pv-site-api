a1028304a628ebfbb21f0caa89855525262bc568
neha-vard <neha.vard@gmail.com>
2023-04-13 18:27:25 -0500

    Create BaseModels for inverter data

--- pv_site_api/pydantic_models.py
+++ pv_site_api/pydantic_models.py
@@ -22,23 +22,30 @@ class PVSiteMetadata(BaseModel):
     """Site metadata"""
 
     site_uuid: str = Field(..., description="Unique internal ID for site.")
-    client_name: str = Field(..., description="Unique name for user providing the site data.")
-    client_site_id: str = Field(..., description="The site ID as given by the providing user.")
+    client_name: str = Field(...,
+                             description="Unique name for user providing the site data.")
+    client_site_id: str = Field(...,
+                                description="The site ID as given by the providing user.")
     client_site_name: str = Field(
         None, decription="The name of the site as given by the providing uuser."
     )
     region: Optional[str] = Field(None, decription="The region of the PV site")
-    dno: Optional[str] = Field(None, decription="The distribution network operator of the PV site.")
-    gsp: Optional[str] = Field(None, decription="The grid supply point of the PV site.")
+    dno: Optional[str] = Field(
+        None, decription="The distribution network operator of the PV site.")
+    gsp: Optional[str] = Field(
+        None, decription="The grid supply point of the PV site.")
     orientation: Optional[float] = Field(
         None, description="The rotation of the panel in degrees. 180° points south"
     )
     tilt: Optional[float] = Field(
         None, description="The tile of the panel in degrees. 90° indicates the panel is vertical."
     )
-    latitude: float = Field(..., description="The site's latitude", ge=-90, le=90)
-    longitude: float = Field(..., description="The site's longitude", ge=-180, le=180)
-    installed_capacity_kw: float = Field(..., description="The site's capacity in kw", ge=0)
+    latitude: float = Field(...,
+                            description="The site's latitude", ge=-90, le=90)
+    longitude: float = Field(...,
+                             description="The site's longitude", ge=-180, le=180)
+    installed_capacity_kw: float = Field(...,
+                                         description="The site's capacity in kw", ge=0)
 
 
 # post_pv_actual
@@ -50,7 +57,8 @@ class PVActualValue(BaseModel):
     """PV Actual Value list"""
 
     datetime_utc: datetime = Field(..., description="Time of data input")
-    actual_generation_kw: float = Field(..., description="Actual kw generation", ge=0)
+    actual_generation_kw: float = Field(...,
+                                        description="Actual kw generation", ge=0)
 
 
 class MultiplePVActual(BaseModel):
@@ -66,8 +74,10 @@ class SiteForecastValues(BaseModel):
     """Forecast value list"""
 
     # forecast_value_uuid: str = Field(..., description="ID for this specific forecast value")
-    target_datetime_utc: datetime = Field(..., description="Target time for forecast")
-    expected_generation_kw: float = Field(..., description="Expected generation in kw")
+    target_datetime_utc: datetime = Field(...,
+                                          description="Target time for forecast")
+    expected_generation_kw: float = Field(...,
+                                          description="Expected generation in kw")
 
 
 # get_forecast
@@ -99,8 +109,10 @@ class PVSites(BaseModel):
 class ClearskyEstimateValues(BaseModel):
     """Clearsky estimate data for a single time"""
 
-    target_datetime_utc: datetime = Field(..., description="Time for clearsky estimate")
-    clearsky_generation_kw: float = Field(..., description="Clearsky estimate in kW", ge=0)
+    target_datetime_utc: datetime = Field(...,
+                                          description="Time for clearsky estimate")
+    clearsky_generation_kw: float = Field(...,
+                                          description="Clearsky estimate in kW", ge=0)
 
 
 class ClearskyEstimate(BaseModel):
@@ -108,4 +120,60 @@ class ClearskyEstimate(BaseModel):
 
     clearsky_estimate: List[ClearskyEstimateValues] = Field(
         ..., description="List of times and clearsky estimate"
+    )
+
+
+class InverterProductionState(BaseModel):
+    """Production State data for an inverter"""
+    productionRate: float = Field(...,
+                                  description="The current production rate in kW")
+    isProducing: bool = Field(
+        ..., description="Whether the solar inverter is actively producing energy or not")
+    totalLifetimeProduction: float = Field(
+        ..., description="The total lifetime production in kWh")
+    lastUpdated: str = Field(
+        ..., description="ISO8601 UTC timestamp of last received production state update")
+
+
+class InverterInformation(BaseModel):
+    """"Inverter information"""
+    id: str = Field(..., description="Solar inverter vendor ID")
+    brand: str = Field(..., description="Solar inverter brand")
+    model: str = Field(..., description="Solar inverter model")
+    siteName: str = Field(..., description="Name of the site, as set by the user on the device/vendor. If no user-specified name is available, we construct a fallback name using the vendor/device/model names.")
+    installationDate: str = Field(...,
+                                  description="Solar inverter installation date")
+
+
+class InverterLocation(BaseModel):
+    """"Longitude and Latitude of inverter"""
+
+    longitude: float = Field(..., description="Longitude in degrees")
+    latitude: float = Field(..., description="Latitude in degrees")
+
+
+class InverterValues(BaseModel):
+    """Inverter Data for a site"""
+
+    id: str = Field(..., description="Solar Inverter ID")
+    vendor: str = Field(
+        ..., description="One of EMA ENPHASE FRONIUS GOODWE GROWATT HUAWEI SMA SOLAREDGE SOLIS")
+    chargingLocationId: str = Field(
+        ..., description="ID of the charging location the solar inverter is currently positioned at (if any).")
+    lastSeen: str = Field(
+        ..., description="The last time the solar inverter was successfully communicated with")
+    isReachable: str = Field(..., description="Whether live data from the solar inverter is currently reachable from Enode's perspective. This 'reachability' may refer to reading from a cache operated by the solar inverter's cloud service if that service has determined that its cache is valid.")
+    productionState: InverterProductionState = Field(
+        ..., description="Descriptive information about the production state")
+    information: InverterInformation = Field(
+        ..., description="Descriptive information about the solar inverter")
+    location: InverterLocation = Field(...,
+                                       description="Solar inverter's GPS coordinates")
+
+
+class Inverters(BaseModel):
+    """Return all Inverter Data"""
+
+    inverters: List[InverterValues] = Field(
+        ..., description="List of inverter data"
     )
