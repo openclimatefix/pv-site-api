ed843753d12124fbff8b3ccd9bccd08e2c483976
Jacky Park <jackypark9852@gmail.com>
2023-04-13 19:17:54 -0500

    Remove print messages

--- pv_site_api/enode_auth.py
+++ pv_site_api/enode_auth.py
@@ -17,12 +17,6 @@ def get_enode_access_token() -> str:
 
     response = httpx.post(url, auth=auth, data=data)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        print("Access token:", response.json()["access_token"])
-    else:
-        print("Error:", response.status_code, response.text)
-
     return response.json()["access_token"]
 
 
