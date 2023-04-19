40690d703eccf01708ffb59b78acca9d1f129bf5
AndrewLester <alester220@gmail.com>
2023-04-19 13:18:13 -0500

    Rename test var for clarity

--- tests/test_enode.py
+++ tests/test_enode.py
@@ -6,15 +6,15 @@ def test_get_enode_link_fake(client, fake):
 
 
 def test_get_enode_link(client, clients, httpx_mock):
-    test_redirect_uri = "https://example.com"
+    test_enode_link_uri = "https://example.com"
 
     httpx_mock.add_response(
         url=f"https://enode-api.production.enode.io/users/{clients[0].client_uuid}/link",
-        json={"linkUrl": test_redirect_uri},
+        json={"linkUrl": test_enode_link_uri},
     )
 
     params = {"redirect_uri": "https://example.org"}
     response = client.get("/enode/link", params=params)
 
     assert response.status_code == 200
-    assert response.json() == test_redirect_uri
+    assert response.json() == test_enode_link_uri
