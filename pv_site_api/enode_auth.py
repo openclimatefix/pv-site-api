03a2785aff3fb2f3d43455c42aa265ab019d86b5
Jacky Park <jackypark9852@gmail.com>
2023-04-20 15:41:11 -0500

    Address PR changes

--- pv_site_api/enode_auth.py
+++ pv_site_api/enode_auth.py
@@ -1,18 +1,19 @@
 import os
+from typing import Optional
 
 import httpx
 
 
 class EnodeAuth(httpx.Auth):
-    def __init__(self, access_token):
-        self.access_token = access_token
-
+    def __init__(self, access_token: Optional[str] = None):
+        self._access_token = access_token
+        
     def auth_flow(self, request):
         # Add the Authorization header to the request using the current access token
         request.headers["Authorization"] = f"Bearer {self.access_token}"
         response = yield request
 
-        if response.status_code == 404:
+        if response.status_code == 401:
             # The access token is no longer valid, refresh it
             token_response = yield self._build_refresh_request()
             self._update_access_token(token_response)
@@ -23,14 +24,14 @@ class EnodeAuth(httpx.Auth):
         return response
 
     def _build_refresh_request(self):
-        # TODO: Add environment variables
         client_id = os.getenv("CLIENT_ID")
-        client_secret = os.getenv("CLIENT_SECRET")
+        client_secret = os.getenv("CLIENT_SECRET")  
         basic_auth = httpx.BasicAuth(client_id, client_secret)
+
         url = os.getenv("ENODE_TOKEN_URL")
         data = {"grant_type": "client_credentials"}
         request = next(basic_auth.auth_flow(httpx.Request("POST", url, data=data)))
-        return request
+        return request  
 
     def _update_access_token(self, response):
         response.read()
