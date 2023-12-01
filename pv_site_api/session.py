bc4ef2050ad554475470a011d3bb3ad0ef56462e
peterdudfield <peter.dudfield@hotmail.com>
2023-12-01 13:07:25 +0000

    only open one session

--- pv_site_api/session.py
+++ pv_site_api/session.py
@@ -4,13 +4,15 @@ import os
 
 from pvsite_datamodel.connection import DatabaseConnection
 
+connection = DatabaseConnection(url=os.getenv("DB_URL", "not_set"))
+
 
 def get_session():
     """Get database settion"""
     if int(os.environ.get("FAKE", 0)):
         yield None
     else:
-        connection = DatabaseConnection(url=os.getenv("DB_URL", "not_set"))
 
         with connection.get_session() as s:
             yield s
+
