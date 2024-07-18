63fc00676c493b5341e1f29825c696d4107c35ad
Henri Dewilde <48734689+Bvr4@users.noreply.github.com>
2024-07-18 18:05:47 +0200

    Add edit_site method (#180)
    * Add put_site_info function and route
    Added put_site_info function that allow users to update metadata of a given site.
    This function call edit_site from pvsite_datamodel
    
    * Add put_site_info route (#178)
    added put_site_info route so a user can update site informations
    added test_put_site_info to test the route
    added test_put_site_and_update to test that informations are actually updated.
    
    * update pvsite-datamodel version (#178)

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -107,37 +107,35 @@ def test_put_site(db_session, client):
     assert sites[1].ml_id == 2
 
 
-# Comment this out, until we have security on this
-# def test_put_site_and_update(db_session):
-#     pv_site = PVSiteMetadata(
-#         site_uuid=str(uuid4()),
-#         client_uuid="eeee-eeee",
-#         client_site_id="the site id used by the user",
-#         client_site_name="the site name",
-#         region="the site's region",
-#         dno="the site's dno",
-#         gsp="the site's gsp",
-#         orientation=180,
-#         tilt=90,
-#         latitude=50,
-#         longitude=0,
-#         installed_capacity_kw=1,
-#         created_utc=datetime.now(timezone.utc).isoformat(),
-#     )
-#
-#     pv_site_dict = json.loads(pv_site.json())
-#
-#     response = client.post(f"sites/", json=pv_site_dict)
-#     assert response.status_code == 200, response.text
-#
-#     pv_site.orientation = 100
-#     pv_site_dict = json.loads(pv_site.json())
-#
-#     response = client.put(f"sites/{pv_site.site_uuid}", json=pv_site_dict)
-#     assert response.status_code == 200, response.text
-#
-#     sites = db_session.query(SiteSQL).all()
-#     assert len(sites) == 1
-#     assert sites[0].site_uuid == pv_site.site_uuid
-#     assert sites[0].orientation == pv_site.orientation
-#
+def test_put_site_and_update(db_session, client):
+    pv_site = PVSiteInputMetadata(
+        client_name="test_client",
+        client_site_id=1,
+        client_site_name="the site name",
+        region="the site's region",
+        dno="the site's dno",
+        gsp="the site's gsp",
+        orientation=180,
+        tilt=90,
+        latitude=50,
+        longitude=0,
+        inverter_capacity_kw=1,
+        module_capacity_kw=1.2,
+        created_utc=datetime.now(timezone.utc).isoformat(),
+    )
+
+    pv_site_dict = json.loads(pv_site.model_dump_json())
+
+    response = client.post("sites/", json=pv_site_dict)
+    assert response.status_code == 201, response.text
+
+    pv_site_put = response.json()
+    site_uuid = pv_site_put["site_uuid"]
+
+    response = client.put(f"sites/{site_uuid}", json={"orientation": 120})
+    assert response.status_code == 200, response.text
+
+    sites = db_session.query(SiteSQL).all()
+    assert len(sites) == 1
+    assert sites[0].orientation == 120
+    assert sites[0].tilt == 90
